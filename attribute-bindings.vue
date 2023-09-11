c7c9758cc6e15f13bd4c50d2b1df351addf066b7
David Chaney <117130933+drchaney@users.noreply.github.com>
2023-09-11 13:14:57 -0500

    Create attribute-bindings.vue

---
+++ attribute-bindings.vue
@@ -0,0 +1,53 @@
+<!--
+Here we are reactively binding element attributes / properties to the state.
+The :title syntax is short for v-bind:title.
+-->
+
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello World!')
+const isRed = ref(true)
+const color = ref('green')
+
+function toggleRed() {
+  isRed.value = !isRed.value
+}
+
+function toggleColor() {
+  color.value = color.value === 'green' ? 'blue' : 'green'
+}
+</script>
+
+<template>
+  <p>
+    <span :title="message">
+      Hover your mouse over me for a few seconds to see my dynamically bound title!
+    </span>
+    <br/>
+    <div :title="color">
+    Because why not?
+	  </div>
+  </p>
+
+  <!--
+  class bindings have special support for objects and arrays
+  in addition to plain strings
+  -->
+  <p :class="{ red: isRed }" @click="toggleRed">
+    This should be red... but click me to toggle it.
+  </p>
+
+	<p :class="{ red: true }"> This is not blue</p>
+
+  <!-- style bindings also support object and arrays -->
+  <p :style="{ color }" @click="toggleColor">
+    This should be green, and should toggle between green and blue on click.
+  </p>
+</template>
+
+<style>
+.red {
+  color: red;
+}
+</style>
